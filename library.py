f10ce2682ec8f7dc1376f736a90269d9b6c1cb87
mcheung008 <62771803+mcheung008@users.noreply.github.com>
2025-05-18 22:59:39 -0700

    Updated threshhold function.

--- library.py
+++ library.py
@@ -996,7 +996,7 @@ def threshold_results(thresh_list, actuals, predicted):
       recall = recall_score(actuals, yhat, zero_division=0)
       f1 = f1_score(actuals, yhat)
       accuracy = accuracy_score(actuals, yhat)
-      auc = roc_auc_score(actuals, predicted)  
+      auc = roc_auc_score(actuals, predicted)
 
       result_df.loc[len(result_df)] = {
           'threshold': t,
@@ -1006,7 +1006,16 @@ def threshold_results(thresh_list, actuals, predicted):
           'accuracy': accuracy,
           'auc': auc
       }
-  fancy_df = result_df.copy()  
+  result_df = result_df.round(2)
+
+  headers = {
+    "selector": "th:not(.index_name)",
+    "props": "background-color: #800000; color: white; text-align: center"
+  }
+  properties = {"border": "1px solid black", "width": "65px", "text-align": "center"}
+
+  fancy_df = result_df.style.highlight_max(color = 'pink', axis = 0).format(precision=2).set_properties(**properties).set_table_styles([headers])
+
   return result_df, fancy_df
 
 
